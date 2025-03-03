f16924982b0c4c96bddb845daf96a88dfa816b3a
Abdelrahman Mohy <a.mohiyyy@gmail.com>
2025-03-04 01:08:33 +0200

    update main url

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,5 @@
 // Main URL
-export const baseURL = `http://127.0.0.1:8000/api`;
+export const baseURL = `https://m-avenue-backend-production.up.railway.app/api`;
 
 // end of auth APIs
 export const registerAPI = `register`;
